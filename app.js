4b09fff25caf5c630a0a3712b41ab86faa0d937b
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2023-09-26 14:49:21 -0400

    database boilerplate

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 import express from "express"
 import {MongoClient, ObjectId} from "mongodb"
-import "dotenv"
+import cookie from "cookie-session"
+import env from "dotenv"
 
 const app = express();
+
+const url =
+    "mongodb+srv://" +
+    process.env.DB_USERNAME +
+    ":" +
+    process.env.DB_PASSWORD +
+    "@" +
+    process.env.DB_HOST;
+
+const dbClient = new MongoClient(url);
+let collection = null;
+let dbname = "";
+let collectionName = ""
+
+const initDatabase = async () => {
+    await dbClient.connect();
+    collection = await dbClient.db(dbname).collection(collectionName);
+}
+initDatabase().then(() => console.log("Connecting to database..."));
+
+
+
