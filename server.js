a9cd86e0441fe156f41a32ac998bcb48bcef81e5
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-09-26 14:43:49 -0400

    Replace requires with import statements

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const express = require("express"),
-    { MongoClient, ObjectId } = require("mongodb"),
-    cookie = require("cookie-session"),
-    app = express(),
-    ejs = require("ejs"),
-    env = require("dotenv").config(),
-    port = 3000;
+import express from "express"
+import {MongoClient, ObjectId} from "mongodb"
+import "dotenv"
 
+const app = express();
