1d7f5a3eeb6918a9a388bbc8b36ade56b1ef9b47
Ansel Chang <ansel.chang@yahoo.com>
2023-09-29 16:42:55 -0400

    top bar

--- src/client/components/top-bar.tsx
+++ src/client/components/top-bar.tsx
@@ -1,7 +1,36 @@
-function Topbar() {
-    const greeting = 'Hello Function Component!';
-  
-    return <h1>{greeting}</h1>;
-  }
-  
-  export default Topbar;
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { COLOR_THEME } from '../color-theme';
+
+const Bar = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: ${COLOR_THEME.TOPBAR};
+color: white;
+height: 80px;
+width: 100%; // Ensure it spans the entire width of the viewport
+position: fixed; // Keep it at the top regardless of scrolling
+top: 0; // Position it at the top of the viewport
+left: 0; // Align it to the left of the viewport
+box-shadow: 0px 3px 10px rgba(0,0,0,0.2);
+z-index: 1000; // Ensure it stacks on top of other elements
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-family: 'Road Rage';
+`;
+
+interface TopBarProps {
+  title: string;
+}
+
+const TopBar: FC<TopBarProps> = ({ title }) => (
+  <Bar>
+    <link href='https://fonts.googleapis.com/css?family=Road Rage' rel='stylesheet'></link>
+    <Title>{title}</Title>
+  </Bar>
+);
+
+export default TopBar;
