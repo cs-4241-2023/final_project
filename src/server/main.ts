622d19464bb9d1a89014865cdfd56718a019bf69
Ansel Chang <ansel.chang@yahoo.com>
2023-09-29 01:57:59 -0400

    working mongoose+react+express

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,34 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from "mongoose";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan('dev')); // log every request to the console
+app.use(express.json()); // requests with json data
+
+// Connect to mongodb
+require('dotenv').config({ path: __dirname+'/.env' });
+const uri = "mongodb+srv://anselychang:" + process.env.MONGODB_PASSWORD + "@ansel.musopg0.mongodb.net/?retryWrites=true&w=majority";
+console.log(uri);
+mongoose.connect(uri, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+} as mongoose.ConnectOptions).then(
+  (result) => {
+
+    // start listening to requests only after we've successfully connected to the database
+    ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
+
+  }
+).catch(
+  (err) => console.log(err)
+);
+
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
-);
+
