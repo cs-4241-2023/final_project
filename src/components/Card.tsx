ab3eb3bae28507d08cc1bb960013b5372305ded8
Flame442 <34169552+Flame442@users.noreply.github.com>
2023-10-04 19:42:14 -0400

    Add Card template

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "../styles/splash.css"
+
+const Card: React.FC = () => {
+
+    return (
+        <>
+            <h3>Card :|</h3>
+        </>
+    );
+};
+
+export default Card;
