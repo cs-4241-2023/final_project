89dba38511ad53e240ac53f0214fdf13281fbcd7
Tucker Raymond <teraymond@wpi.edu>
2023-10-05 17:33:41 -0400

    Example stuff and delete crap
    Example on how to make a page and add components to the page. Also deleted a lot of unnecessary stuff

---
+++ src/app/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Header extends React.Component{
+    render(){
+        let a = 17;
+        return(
+           <div>
+            <h1>{a}</h1>
+        </div> 
+        )
+    }
+}
---
+++ src/app/components/page.js
@@ -0,0 +1,13 @@
+'use client'
+import React, {Componenet} from "react"
+import Header from './header'
+
+export default function Component(){
+    return(
+        <div>
+            <h1>WhAt'S tHe DeAl</h1>
+            <Header></Header>
+            <Header></Header>
+        </div>
+    )
+}
