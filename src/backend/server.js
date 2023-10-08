472a95046cbb51ef5fa6b67546f290b7780d6c1b
Randy Huang <huangyulu.zh@gmail.com>
2023-10-08 00:33:53 -0400

    2nd merge resolve

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,6 @@
-import express from "express"
+import express, { response } from "express"
 import ViteExpress from "vite-express"
-import { MongoClient, ObjectId } from "mongodb"
+import {MongoClient, ObjectId} from "mongodb"
 import cookie from "cookie-session"
 import env from "dotenv"
 const app = express();
@@ -8,15 +8,16 @@ env.config();
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`;
 const dbClient = new MongoClient(url);
+let currCollection = null
 
-let usersCollection = null;
+let usersCollection = null
 let allCollections = null;
 
 const initDatabase = async () => {
     await dbClient.connect();
     usersCollection = await dbClient.db("RendezViewDatabase").collection("Users");
     allCollections = await dbClient.db("RendezViewDatabase").collections();
-    if (usersCollection !== null && allCollections !== null) {
+    if(usersCollection !== null && allCollections !== null) {
         return 0;
     } else {
         return -1;
@@ -24,15 +25,15 @@ const initDatabase = async () => {
 }
 
 initDatabase().then((result) => {
-    if (result === 0) {
+    if(result === 0) {
         console.log("Connected to database");
     } else {
         console.error("Unable to connect to database")
     }
 });
 
-app.use((request, response, next) => {
-    if (usersCollection !== null) {
+app.use((request, response, next)  => {
+    if(usersCollection !== null) {
         next();
     } else {
         response.sendStatus(503); // send 503 on database disconnect
@@ -45,7 +46,38 @@ app.use(cookie({
     keys: ["key1", "key2"]
 }));
 
-app.get("/users", async (request, response) => {
+app.post("/login", async (req, res) => {
+    // Find user within MongoDB
+    console.log(req.body.usernames)
+
+    const response = await usersCollection.findOne({
+        username: req.body.username,
+    });
+
+    console.log(response)
+
+    if (response !== null) {
+        if(response.password === req.body.password) {
+            // TODO: figure out the cookie because brandon is stupid
+            req.session.user = req.body.username
+            console.log("login success")
+            res.status(200);
+            res.end();
+        }
+        else {
+            console.log("incorrect password");
+            res.status(401);
+            res.end();
+        }
+    }
+    else {
+        console.log("user not found") 
+        res.status(404);
+        res.end
+    }
+})
+
+app.get("/get-users", async (request, response) => {
     let result = await usersCollection.find({}).toArray();
     response.send(result)
 });
@@ -53,14 +85,14 @@ app.get("/users", async (request, response) => {
 app.post("/get-collection", async (request, response) => {
     let requestedCollection = null;
     allCollections.forEach(collection => {
-        if (collection.namespace === `RendezViewDatabase.${request.body.requestedCollection}`) {
-            requestedCollection = collection;
-        }
-    });
+       if(collection.namespace === `RendezViewDatabase.${request.body.requestedCollection}`) {
+           requestedCollection = collection;
+       }
+   });
 
-    if (requestedCollection !== null) {
+    if(requestedCollection !== null) {
         let data = await requestedCollection.find({}).toArray();
-        response.writeHead(200, { "Content-Type": "application/json" });
+        response.writeHead(200, {"Content-Type": "application/json"});
         response.end(JSON.stringify(data));
     } else {
         response.sendStatus(404);
@@ -83,7 +115,7 @@ app.post("/add-group", async (request, response) => {
         meetingTimes: "TBD"
     });
 
-    response.writeHead(200, { "Content-Type": "application/json" });
+    response.writeHead(200, {"Content-Type": "application/json"});
     response.end(JSON.stringify({}));
 });
 
@@ -101,8 +133,8 @@ app.delete("/delete-group", (request, response) => {
     requestedCollection.deleteOne({
         _id: new ObjectId(request.body._id)
     });
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify({ result: "Success", message: "" }));
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify({result: "Success", message: ""}));
 });
 
-ViteExpress.listen(app, process.env.PORT)
+ViteExpress.listen(app, parseInt(process.env.PORT))
