712cfbe313834038ff680a490b16b9ef625b829c
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-10-08 02:52:46 -0400

    Add basic creation of users

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -1,39 +1,42 @@
-import express, { response } from "express"
-import ViteExpress from "vite-express"
-import {MongoClient, ObjectId} from "mongodb"
-import cookie from "cookie-session"
-import env from "dotenv"
+import express, { response } from "express";
+import ViteExpress from "vite-express";
+import { MongoClient, ObjectId } from "mongodb";
+import cookie from "cookie-session";
+import env from "dotenv";
 const app = express();
 env.config();
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`;
 const dbClient = new MongoClient(url);
-let currCollection = null
+let db = null;
+let currCollection = null;
 
-let usersCollection = null
+let usersCollection = null;
 let allCollections = null;
 
 const initDatabase = async () => {
     await dbClient.connect();
-    usersCollection = await dbClient.db("RendezViewDatabase").collection("Users");
+    db = await dbClient.db("RendezViewDatabase")
+    usersCollection = await db
+        .collection("Users");
     allCollections = await dbClient.db("RendezViewDatabase").collections();
-    if(usersCollection !== null && allCollections !== null) {
+    if (usersCollection !== null && allCollections !== null) {
         return 0;
     } else {
         return -1;
     }
-}
+};
 
 initDatabase().then((result) => {
-    if(result === 0) {
+    if (result === 0) {
         console.log("Connected to database");
     } else {
-        console.error("Unable to connect to database")
+        console.error("Unable to connect to database");
     }
 });
 
-app.use((request, response, next)  => {
-    if(usersCollection !== null) {
+app.use((request, response, next) => {
+    if (usersCollection !== null) {
         next();
     } else {
         response.sendStatus(503); // send 503 on database disconnect
@@ -41,10 +44,12 @@ app.use((request, response, next)  => {
 });
 
 app.use(express.json());
-app.use(cookie({
-    name: "session",
-    keys: ["key1", "key2"]
-}));
+app.use(
+    cookie({
+        name: "session",
+        keys: ["key1", "key2"],
+    })
+);
 
 app.post("/login", async (req, res) => {
     // Find user within MongoDB
@@ -53,42 +58,69 @@ app.post("/login", async (req, res) => {
     });
 
     if (response !== null) {
-        if(response.password === req.body.password) {
+        if (response.password === req.body.password) {
             // TODO: figure out the cookie because brandon is stupid
-            req.session.user = req.body.username
-            console.log("login success")
+            req.session.user = req.body.username;
+            console.log("login success");
             res.status(200);
             res.end();
-        }
-        else {
+        } else {
             console.log("incorrect password");
             res.status(401);
             res.end();
         }
-    }
-    else {
-        console.log("user not found") 
+    } else {
+        console.log("user not found");
         res.status(404);
         res.end();
     }
-})
+});
+
+app.post("/signup", async (req, res) => {
+    const response = await usersCollection.findOne({
+        username: req.body.username,
+    });
+
+    // If the username is not already taken, add the user into the 'User' collection and create a new collection for the user
+    if (response === null) {
+        // TODO: Might want to encase this in a try catch
+        // Create a new collection with the username (can change this to a new MongoDB _id instead)
+        await db.createCollection(req.body.username);
+
+        // Insert a new document into the 'Users' collection
+        usersCollection.insertOne({
+            username: req.body.username,
+            password: req.body.password,
+            collectionName: req.body.username,
+        });
+
+        res.status(200);
+        res.end();
+    } else {
+        res.status(401);
+        res.end();
+    }
+});
 
 app.get("/get-users", async (request, response) => {
     let result = await usersCollection.find({}).toArray();
-    response.send(result)
+    response.send(result);
 });
 
 app.post("/get-collection", async (request, response) => {
     let requestedCollection = null;
-    allCollections.forEach(collection => {
-       if(collection.namespace === `RendezViewDatabase.${request.body.requestedCollection}`) {
-           requestedCollection = collection;
-       }
-   });
+    allCollections.forEach((collection) => {
+        if (
+            collection.namespace ===
+            `RendezViewDatabase.${request.body.requestedCollection}`
+        ) {
+            requestedCollection = collection;
+        }
+    });
 
-    if(requestedCollection !== null) {
+    if (requestedCollection !== null) {
         let data = await requestedCollection.find({}).toArray();
-        response.writeHead(200, {"Content-Type": "application/json"});
+        response.writeHead(200, { "Content-Type": "application/json" });
         response.end(JSON.stringify(data));
     } else {
         response.sendStatus(404);
@@ -96,10 +128,12 @@ app.post("/get-collection", async (request, response) => {
 });
 
 app.post("/add-group", async (request, response) => {
-
     let requestedCollection = null;
-    allCollections.forEach(collection => {
-        if (collection.namespace === `RendezViewDatabase.${request.body.collection}`) {
+    allCollections.forEach((collection) => {
+        if (
+            collection.namespace ===
+            `RendezViewDatabase.${request.body.collection}`
+        ) {
             requestedCollection = collection;
         }
     });
@@ -108,29 +142,31 @@ app.post("/add-group", async (request, response) => {
         groupName: request.body.groupName,
         groupDescription: request.body.groupDescription,
         groupUsers: request.body.groupUsers,
-        meetingTimes: "TBD"
+        meetingTimes: "TBD",
     });
 
-    response.writeHead(200, {"Content-Type": "application/json"});
+    response.writeHead(200, { "Content-Type": "application/json" });
     response.end(JSON.stringify({}));
 });
 
 app.delete("/delete-group", (request, response) => {
-
     let requestedCollection = null;
-    allCollections.forEach(collection => {
-        if (collection.namespace === `RendezViewDatabase.${request.body.collection}`) {
+    allCollections.forEach((collection) => {
+        if (
+            collection.namespace ===
+            `RendezViewDatabase.${request.body.collection}`
+        ) {
             requestedCollection = collection;
         }
     });
 
-    console.log("Delete Request for ID: " + request.body._id)
+    console.log("Delete Request for ID: " + request.body._id);
 
     requestedCollection.deleteOne({
-        _id: new ObjectId(request.body._id)
+        _id: new ObjectId(request.body._id),
     });
-    response.writeHead(200, {"Content-Type": "application/json"});
-    response.end(JSON.stringify({result: "Success", message: ""}));
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ result: "Success", message: "" }));
 });
 
-ViteExpress.listen(app, parseInt(process.env.PORT))
+ViteExpress.listen(app, parseInt(process.env.PORT));
