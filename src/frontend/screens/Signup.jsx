6239fa9fe061eccc783a4a9f7c6540825035bb1b
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-10-08 02:39:03 -0400

    Add front-end signup
    Added a button on the login screen to go to the sign-up page if needed.
    Also added more state variables to control error messages and the header depending on whether or not the user is on the login/sign up page. Sign-up page also has password confirmation before sending a fetch request.

---
+++ src/frontend/screens/Signup.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function Signup(props) {
+    const { setSignUp, setErrorMessage, setHeaderMessage } = props;
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    return (
+        <>
+            <form>
+                <input
+                    type="text"
+                    placeholder="username"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="confirm password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button type="submit" onClick={(e) => handleSignup(e)}>
+                    Sign up!
+                </button>
+            </form>
+        </>
+    );
+
+    function handleSignup(e) {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setErrorMessage("Passwords do not match.")
+            return;
+        }
+
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }).then((response) => {
+            if (response.status === 200) {
+                setSignUp(false);
+                setHeaderMessage("Login To RendezView")
+            }
+            else if (response.status === 401) {
+                setErrorMessage("Username taken.")
+            }
+        });
+    }
+}
