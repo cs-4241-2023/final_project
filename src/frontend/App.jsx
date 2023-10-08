d3ed7ef201eff1094df8e43c36de01a28ed03d4a
Randy Huang <huangyulu.zh@gmail.com>
2023-10-08 15:04:04 -0400

    fix merge bugs

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -2,17 +2,14 @@ import { useState } from "react";
 import Login from "./screens/Login";
 import Dashboard from "./screens/Dashboard"
 import "./css/styles.css"
-// import "./css/Login.css"
-
-
 
 function App() {
     const [loggedIn, setLoggedIn] = useState(false);
     console.log("LOGGED IN:", loggedIn)
-    
+
     return (
         <div className="container">
-            {!loggedIn ? <Login setLoggedIn={setLoggedIn} /> : <Dashboard setLoggedIn={setLoggedIn} />} 
+            {!loggedIn ? <Login setLoggedIn={setLoggedIn} /> : <Dashboard setLoggedIn={setLoggedIn} />}
         </div>
     );
 }
