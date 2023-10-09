f0e5404f2274e4d0e542ea998eb71073cce07bd3
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-10-09 12:30:03 -0400

    express server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+// logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
+
+// parses JSON bodies
+app.use(express.json());
+
+// all other routes and middleware below
+//test route
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = 3000;
+ViteExpress.listen(app, port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
