4b23c6c1d2cc0e53c4a748f61702d86413b0b77c
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-10-09 14:02:06 -0400

    Include header sections

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,21 @@ import "../../App.css";
 function Header() {
   return (
     <>
-      <div className="header"></div>
+      <div className="header">
+        <div className="greeting-card">
+          <div className="greeting-text">Hello, User!</div>
+          <div className="greeting-info">
+            {"time"} | {"date"}
+          </div>
+        </div>
+        <div className="weather-card">
+          <span class="material-symbols-outlined">sunny</span>
+          <div className="weather-info">
+            <div className="degrees">78°F</div>
+            <div className="weather-description">Sunny</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
