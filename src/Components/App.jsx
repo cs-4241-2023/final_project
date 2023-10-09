adeffb034a897be57141b6b3b5f33a7d97378668
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-10-09 13:23:16 -0400

    Include CSS Reset template and starter code for components

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,14 @@
 import "../../App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
-      <div>Testc</div>
+      <div className="main">
+        <Header />
+        <Sidebar />
+      </div>
     </>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import "../../App.css";
+
+function Header() {
+  return (
+    <>
+      <div className="header"></div>
+    </>
+  );
+}
+
+export default Header;
