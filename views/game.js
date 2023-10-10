e46743698419685df1e5e453ae5d803adc8bdac5
AlexMarrinan <alex@balroe.com>
2023-10-10 17:39:22 -0400

    score + upgrade shop

--- views/game.js
+++ views/game.js
@@ -1,5 +1,18 @@
+var score = 0;
+var rate = 0;
+var scoretext;
+var ratetext;
+var icon;
+
+let upgradeIcons = []
+let upgradeCosts = [50, 300]
+let upgradeRates = [5, 20]
+
+let frames = 120
+let currentFrame = 120;
 class Example extends Phaser.Scene
-{
+{      
+
     preload ()
     {
         // this.load.setBaseURL('https://labs.phaser.io');
@@ -9,27 +22,82 @@ class Example extends Phaser.Scene
         // this.load.image('red', 'assets/particles/red.png');
 
         this.load.image('f1blue', 'assets/f1blue.png')
+        this.load.image('f1green', 'assets/f1green.png')
+        this.load.image('f1red', 'assets/f1red.png')
+
     }
 
     create ()
     {
-        this.add.image(400, 300, 'sky');
+        //this.add.image(400, 300, 'sky');
+        
+        scoretext = this.add.text(0, 0, `Points: ${score}`, { fontSize: 50 });
+        ratetext = this.add.text(0, 50 , `Rate: ${rate}/sec`, { fontSize: 50 });
 
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+        // const particles = this.add.particles(500, 400, 'f1blue', {
+        //     speed: 100,
+        //     scale: { start: 0.75, end: 0 },
+        // });
 
-        const logo = this.physics.add.image(400, 100, 'f1blue');
+        icon = this.add.sprite(500, 400, 'f1blue').setInteractive();
+        icon.on('pointerdown', this.onClick)
+        // icon.setVelocity(0, 0);
+        // icon.setBounce(1, 1);
+        // icon.setCollideWorldBounds(true);
+        icon.setScale(0.7, 0.7);
+        this.add.text(50, 550, `Upgrade Shop:`, {fontSize: 30});
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setScale(0.5, 0.5);
-        logo.setCollideWorldBounds(true);
+        upgradeIcons[0] = this.add.sprite(80    , 650, 'f1green').setInteractive();
+        upgradeIcons[0].setScale(0.25, 0.25);
+        upgradeIcons[0].setAlpha(0.25);   
+        upgradeIcons[0].on('pointerdown', () => {onUpgrade(0)})
 
-        particles.startFollow(logo);
+        this.add.text(50, 700, `Cost: ${upgradeCosts[0]}`);
+        this.add.text(50, 720, `Rate: +${upgradeRates[0]}`);
+
+        upgradeIcons[1] = this.add.sprite(230, 650, 'f1red').setInteractive();
+        upgradeIcons[1].setScale(0.25, 0.25);
+        upgradeIcons[1].setAlpha(0.25);   
+        upgradeIcons[1].on('pointerdown', () => {onUpgrade(1)})
+
+        this.add.text(200, 700, `Cost: ${upgradeCosts[1]}`);
+        this.add.text(200, 720, `Rate: +${upgradeRates[1]}`);
     }
+    update (){
+        for (var i = 0; i < upgradeCosts.length; i++){
+            if (score > upgradeCosts[i]){
+                upgradeIcons[i].setAlpha(1.0);
+            }else{
+                upgradeIcons[i].setAlpha(0.25);
+            }
+        }
+        if (currentFrame <= 0){
+            score += rate;
+            currentFrame = frames;
+        }else{
+            currentFrame--;
+        }
+        scoretext.text = `Points: ${score}`
+        ratetext.text = `Rate: ${rate}/sec`
+    }
+    onClick () {
+        score++;
+        // const particles = this.add.particles(500, 400, 'f1blue', {
+        //     speed: 100,
+        //     scale: { start: 0.75, end: 0 },
+        // });
+    }
+}
+
+function onUpgrade (index) {
+    const cost = upgradeCosts[index],
+          addToRate = upgradeRates[index];
+
+    if (score < cost){
+        return;
+    }
+    score -= cost;
+    rate += addToRate;
 }
 
 const config = {
@@ -37,13 +105,13 @@ const config = {
     // width: 1600,
     // height: 1200,
     scale: {
-        mode: Phaser.Scale.AUTO
+        mode: Phaser.Scale.FIT
     },
     scene: Example,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 0 }
         }
     }
 };
