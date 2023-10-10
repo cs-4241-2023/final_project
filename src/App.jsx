7352f05991fc26278484a06f54bf36065be93319
randyer <rbdyer3@gmail.com>
2023-10-10 18:12:06 -0400

    UI done and validation for characters and word length done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import GameBoard from "./components/gameboard";
 
+
 function App() {
-  let [lettersArray, setLettersArray] = useState(["G", "O", "A", "T", "S"]);
-  setTimeout(() => {
-    setLettersArray(["G", "O", "A", "T", "S", "!", "!", "!"]);
-  }, 2000);
+  let [lettersArray, setLettersArray] = useState(["A", "B", "C", "D", "E","F", "G"]);
   return (
     <>
       <h1>Spelling Goat</h1>
       <GameBoard lettersArray={lettersArray} />
+    
     </>
   );
 }
