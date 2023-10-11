ba3fa2d5d9d9a3944bb6af370630410cd129df2a
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-10-11 15:46:25 -0400

    protecting routes using auth works now

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GameBoard from "./components/gameboard";
 import PuzzleMenu from "./components/Menu";
 import Score from "./components/Score";
 import GuessList from "./components/GuessList";
 import Scoreboard from "./components/ScoreBoard";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
 function Home() {
+  //auth stuff
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const verifyCookie = async () => {
+      //first check if cookie exists
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      // then check if the cookies token is authorized
+      fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": cookies.token,
+        },
+        credentials: "include",
+      })
+        .then((res) => {
+          console.log("response", res);
+          if (res.status === 200) {
+            console.log("cookie authentication successful");
+            return res.json();
+          } else {
+            console.log("cookie authentication failed");
+            navigate("/login");
+          }
+        })
+        .then((data) => {
+          console.log("data", data);
+          setUsername(data.user);
+        })
+        .catch((err) => {
+          console.log("cookie authentication failed: ", err);
+          navigate("/login");
+        });
+    };
+
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
+  //actual page content below
   let [lettersArray, setLettersArray] = useState([
     "A",
     "B",
