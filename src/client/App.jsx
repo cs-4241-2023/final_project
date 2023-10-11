e1ee55a1749ca62e4aca2e66729c0b4b25f6e47b
scriblesean <sean.arackal@gmail.com>
2023-10-11 18:12:18 -0400

    feat: styles
    user_login, user_creation, landing_page styles added

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,35 +1,41 @@
-import React from "react"
-import LandingPage from './landing_page/landing_page.jsx'
-import NavBar from './nav_bar/nav_bar.jsx'
-import DisplayNavBar from './nav_bar/display_nav_bar.jsx'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom' //Router provides a roter for use in web broswers and provides clean URL paths.
-import MusicTourBuilder from './music_tour_builder/music_tour_builder.jsx'
-import MusicTrendsInformation from './music_trends_information/music_trends_information.jsx'
+import React from "react";
+import LandingPage from "./landing_page/landing_page.jsx";
+import NavBar from "./nav_bar/nav_bar.jsx";
+import DisplayNavBar from "./nav_bar/display_nav_bar.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Router provides a roter for use in web broswers and provides clean URL paths.
+import MusicTourBuilder from "./music_tour_builder/music_tour_builder.jsx";
+import MusicTrendsInformation from "./music_trends_information/music_trends_information.jsx";
 
 //JSX converts HTML into React elements
 //Files grouped by feature
 
 function App() {
-
   return (
     <>
-      <Router> 
-          <DisplayNavBar>
-            <NavBar /> 
-          </DisplayNavBar> 
-          <Routes> 
-            <Route path = '/' element = {<LandingPage/>} /> 
-            <Route path = '/music_tour_builder' element = {<MusicTourBuilder/>} /> 
-            <Route path = '/music_trends_information' element = {<MusicTrendsInformation/>} /> 
-          </Routes> 
+      <Router>
+        <DisplayNavBar>
+          <NavBar />
+        </DisplayNavBar>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/music_tour_builder" element={<MusicTourBuilder />} />
+          <Route
+            path="/music_trends_information"
+            element={<MusicTrendsInformation />}
+          />
+        </Routes>
       </Router>
-      <footer>
-        <div>
-          Fantasy Music Tour Builder created by Matthew McAlarney, Sean Arackal, Adish Jain, Daniel Onyema, and Sultan Adedeji
-        </div>
-      </footer>
-  </>
-  )
+      <div class="container hero is-fullheight">
+        <footer class="footer has-text-centered is-flex-align-items-flex-end mt-auto">
+          <div>
+            <strong>Fantasy Music Tour Builder</strong> created by Matthew
+            McAlarney, Sean Arackal, Adish Jain, Daniel Onyema, and Sultan
+            Adedeji
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/client/user_login/user_login.jsx
+++ src/client/user_login/user_login.jsx
@@ -1,78 +1,105 @@
-import React, {useState, useEffect, useRef} from 'react' //useRef provides references to component instances.
-import {useNavigate} from 'react-router-dom'
+import React, { useState, useEffect, useRef } from "react"; //useRef provides references to component instances.
+import { useNavigate } from "react-router-dom";
+import "bulma/css/bulma.min.css";
 
 function userLogin() {
+  //The state object is where you store property values that belong to the component.
+  //When the state object changes, the component re-renders.
+  const [userLoginFeedbackText, setUserLoginFeedbackText] = useState("");
+  const navigate = useNavigate();
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
 
-    //The state object is where you store property values that belong to the component.
-    //When the state object changes, the component re-renders.
-    const [userLoginFeedbackText, setUserLoginFeedbackText] = useState("")
-    const navigate = useNavigate()
-    const usernameInputRef = useRef()
-    const passwordInputRef = useRef()
+  //Use fetch await for form validation
+  //Use bcrypt client-side before sending data to server
 
-    //Use fetch await for form validation
-    //Use bcrypt client-side before sending data to server
+  useEffect(() => {
+    console.log(userLoginFeedbackText);
+  }, [userLoginFeedbackText]);
 
-    useEffect(() => {
-        console.log(userLoginFeedbackText)
-    }, [userLoginFeedbackText])
+  async function handleLoginSubmit(event) {
+    event.preventDefault();
 
-    async function handleLoginSubmit(event) {
-        event.preventDefault()
-        
-        const loginUsername = usernameInputRef.current.value
-        const loginPassword = passwordInputRef.current.value
+    const loginUsername = usernameInputRef.current.value;
+    const loginPassword = passwordInputRef.current.value;
 
-        const response = await fetch('/userLogin', { 
-            method: 'POST', 
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                username: loginUsername,
-                password: loginPassword
-            }) 
-        })
-            
-        const message = await response.text() //Here, JSON is parsed to produce a JavaScript object  
-        //const message = JSON.stringify(serverMessage)
-       
-        if(message === "SuccessfulUserAuthentication.") {
-            //Redirect to page with main functionality.
-            navigate("/music_tour_builder")
-        } else {
-            if(message === "MissingInformation") {
-                setUserLoginFeedbackText("The login information you submitted is invalid. There is missing information in at least one input field.")
-            } else if(message === "WhitespacePresent") {
-                setUserLoginFeedbackText("The login information you submitted is invalid. Both the username and password cannot contain any whitespace.")
-            } else if(message === "UserNotFound") {
-                setUserLoginFeedbackText("User not found.")
-            } else if(message === "IncorrectPassword") {
-                setUserLoginFeedbackText("Incorrect password entered.")
-            } else if(message === "InternalServerError") {
-                setUserLoginFeedbackText("There was an internal server error that prevented successful login.")
-            }         
-        }
+    const response = await fetch("/userLogin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: loginUsername,
+        password: loginPassword,
+      }),
+    });
 
-        console.log(message)  
+    const message = await response.text(); //Here, JSON is parsed to produce a JavaScript object
+    //const message = JSON.stringify(serverMessage)
+
+    if (message === "SuccessfulUserAuthentication.") {
+      //Redirect to page with main functionality.
+      navigate("/music_tour_builder");
+    } else {
+      if (message === "MissingInformation") {
+        setUserLoginFeedbackText(
+          "The login information you submitted is invalid. There is missing information in at least one input field."
+        );
+      } else if (message === "WhitespacePresent") {
+        setUserLoginFeedbackText(
+          "The login information you submitted is invalid. Both the username and password cannot contain any whitespace."
+        );
+      } else if (message === "UserNotFound") {
+        setUserLoginFeedbackText("User not found.");
+      } else if (message === "IncorrectPassword") {
+        setUserLoginFeedbackText("Incorrect password entered.");
+      } else if (message === "InternalServerError") {
+        setUserLoginFeedbackText(
+          "There was an internal server error that prevented successful login."
+        );
+      }
     }
-    
-    return (
+
+    console.log(message);
+  }
+
+  return (
+    <div class="box, columns is-mobile is-centered">
+      <form>
         <div>
-            <h2>
-                Login
-            </h2>
-            <form>
-                <div>
-                    <label htmlFor = "un">Username</label>
-                    <input type = "text" id = "un" ref = {usernameInputRef} name = "username"/>
-                </div>
-                <div>
-                    <label htmlFor = "pw">Password</label>
-                    <input type = "password" id = "pw" ref = {passwordInputRef} name = "password"/>
-                </div>
-                    <button onClick = {handleLoginSubmit}>Login</button>
-            </form>
-            <p>{userLoginFeedbackText}</p>
-        </div> )
+          <label class="label has-text-centered" htmlFor="un">
+            Username
+          </label>
+          <input
+            class="input is-primary  input is-rounded"
+            type="text"
+            id="un"
+            ref={usernameInputRef}
+            name="username"
+          />
+        </div>
+        <div>
+          <label class="label has-text-centered" htmlFor="pw">
+            Password
+          </label>
+          <input
+            class="input is-primary  input is-rounded"
+            type="password"
+            id="pw"
+            ref={passwordInputRef}
+            name="password"
+          />
+        </div>
+        <div class="block">
+          <p class="pt-5"> </p>
+          <div class="columns is-mobile is-centered">
+            <button class="button is-info " onClick={handleLoginSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
+      <p>{userLoginFeedbackText}</p>
+    </div>
+  );
 }
 
-export default userLogin
+export default userLogin;
