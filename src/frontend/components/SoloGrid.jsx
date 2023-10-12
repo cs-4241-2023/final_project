e7857311dd31541e12efb4a0867d86387bd52f07
SamKarkache <swkarkache@wpi.edu>
2023-10-12 19:08:20 -0400

    removed bad import

--- src/frontend/components/SoloGrid.jsx
+++ src/frontend/components/SoloGrid.jsx
@@ -1,6 +1,5 @@
 import { set } from 'mongoose';
 import React, { useEffect, useState } from 'react';
-import { send } from 'vite';
 
 const timeSlots = ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM'];
 
