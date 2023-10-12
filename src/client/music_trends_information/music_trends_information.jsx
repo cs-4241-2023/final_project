e1879437cf0d7fb8e8843a104ed60a9c818d4e17
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2023-10-11 22:50:18 -0400

    Moved Adish's HTML for music trends information page over to main

--- src/client/music_trends_information/music_trends_information.jsx
+++ src/client/music_trends_information/music_trends_information.jsx
@@ -2,58 +2,45 @@ import React from 'react'
 
 function musicTrendsInformation() {
     return (
-            <>
-            <div className="App">
-                <h1>Dropdown Demo</h1>
-                {/* <select>
-                    <option value="New Genres" >New Genres</option>
-                    <option value="Revived Genres">Revived Genres</option>
-                    <option value="Technology in Music" >Technology in Music</option>
-                </select> */}
-                <div>
-    
-                    <Link to = "/newGenre">
-                        <button>New Genres</button>
-                    </Link>
-                    <Link to = "/revivedGenres">
-                        <button>Revived Genres</button>
-                    </Link>
-                    <Link to = "/technology">
-                        <button>Technology in Music</button>
-                    </Link>
-    
-                </div>
-                
-               
-                
-                {/* <button onClick = {handleRevived}>Revived Genres</button>
-                <button onClick = {handleTechnology}>Technology in Music</button> */}
-            </div>
-            <h2>Genre Trend</h2>
-            <p> https://daigr.am/d58e7166.svg</p>
-            <h2>New Genres</h2>
-            <h3>Jersey Drill </h3>
-            <p>From New Jersey, this genre is a regional variant of UK drill, combining aggressive beats with distinct local music </p>
-            <h3>TikTok Pop</h3>
-            <p>Driven by the virality on the TikTok platform, this genre showcases songs and artists that gain popularity through viral challenges and trends on the app</p>
-            <h3>Eco-Folk</h3>
-            <p>Eco-Folk genre artists craft songs around themes of sustainability, conservation, and the beauty of nature. </p>
-    
-            <h2>Revived Genres</h2>
-            <h3>70s Rock</h3>
-            <p>This genre has come back due to film sound tracks like "Guardians of The Galaxy" </p>
-            <h3>Blues Revival</h3>
-            <p>The blues genre saw a revival in 2023, as seen in radio shows and other platforms  </p>
-            <h3>R&B Revival</h3>
-            <p>R&B has made a return in 2023, having nostalgic emotion while also adding new twists to keep the genre alive </p>
-            <h2>Technology in Music</h2>
-            <h3>AI-Generated Music</h3>
-            <p>This genre melds AI with different music, allowing computers to compose new music, blurring the lines between human and machine created music.</p>
-            <h3>Music Consumption</h3>
-            <p>The shift from analog to digital music has fundamentally changed the way that we consume music, allowing users to listen to music anytime, anywhere.</p>
-            <h3>Virtual Reality</h3>
-            <p> VR technologies are being used to create musical experiences,including virtual concerts which became particularly popular during the pandemic era </p>
-            </>
+        <div>
+            <section>
+                <h2>Music Trends Information</h2>
+                <p>Allow the following information about the current state of music to influence your fantasy music tour!</p>
+            </section>
+            <section>
+                <h3>New Genres</h3>
+                    <h4>Jersey Drill</h4>
+                        <p>From New Jersey, this subgenre of Hip Hop and Rap is a regional variant of UK Drill, combining aggressive beats with distinct local music.</p>
+                    <h4>TikTok Pop</h4>
+                        <p>Driven by the high level of virality on TikTok, TikTok Pop showcases songs and artists that gain popularity through viral challenges and media trends on the platform.</p>
+                    <h4>Eco-Folk</h4>
+                        <p>Eco-Folk artists craft songs around themes of sustainability, conservation, and the beauty of nature.</p>
+            </section>
+            <section>
+                <h3>Revived Genres</h3>
+                    <h4>70s Rock Revival</h4>
+                        <p>70s Rock has come back due to film sound tracks like "Guardians of The Galaxy".</p>
+                    <h4>Blues Revival</h4>
+                        <p>Blues saw a revival in 2023, as heard on radio shows and other media platforms.</p>
+                    <h4>R&B Revival</h4>
+                        <p>R&B has made a return in 2023, carrying a great deal of nostalgic emotion while also adding new twists to keep the genre alive.</p>
+                    <h4>Nu Metal Revival</h4>
+                        <p>Nu Metal along with its distinctive fashion has been coming back in great strides over the past 5 years. As of 2023, the genre has attracted a new generation of younger fans in their teens and 20s.</p>
+            </section>
+            <section>
+                <h3>Technology in Music</h3>
+                    <h4>AI-Generated Music</h4>
+                        <p>This genre melds AI with a variety of music, allowing computers to compose new music, blurring the lines between music created by humans and machines.</p>
+                    <h4>Music Consumption</h4>
+                        <p>The shift from analog to digital music during the 21st century has fundamentally changed the way that we consume music, allowing users to listen to music on new platforms that continue to evolve.</p>
+                    <h4>Virtual Reality</h4>
+                        <p>Virtual reality technologies are being used to create new musical experiences, including virtual concerts which became particularly popular during the COVID-19 Pandemic.</p>
+            </section>
+            <section>
+                <h2>Listen to the Billboard Hot 100 this week and identify artists: Top 40 Songs of 2022 - 2023</h2>
+                <iframe width="930" height="523" src="https://www.youtube.com/embed/ixJPEVaxH3o" title="Top 40 Songs of 2022 2023 - Billboard Hot 100 This Week - Best Pop Music Playlist on Spotify 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </section>
+        </div>
     )
 }
 
